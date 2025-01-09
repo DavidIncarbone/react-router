fabb6e087b783413645b319aa01bb00a791f4b21
DavidIncarbone <david.incarbone@gmail.com>
2025-01-09 15:11:40 +0100

    Create navbar component and import in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,10 @@
-import { Link, NavLink } from "react-router-dom";
+
+import Navbar from "./Navbar";
 
 function Header() {
 
     return <header>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <NavLink to="/" className="navbar-brand">HomePage</NavLink>
-
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-
-                    </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
-        </nav>
+        <Navbar />
         <h1 className="text-center p-3">Il mio blog</h1>
     </header>
 }
