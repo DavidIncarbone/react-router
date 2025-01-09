d95da5290593256d6cc292f49a6cc91535320742
DavidIncarbone <david.incarbone@gmail.com>
2025-01-09 15:14:25 +0100

    Add about link on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,10 +12,10 @@ export default function Navbar() {
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <NavLink to="/posts" className="nav-link active" aria-current="page">Lista dei posts</NavLink>
-
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <NavLink to="about" className="nav-link">Chi siamo</NavLink>
+
                         </li>
 
                     </ul>
