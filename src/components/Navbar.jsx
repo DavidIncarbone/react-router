34a698ecfe156a2ec4acd3a94af26be7c1b9d4b0
DavidIncarbone <david.incarbone@gmail.com>
2025-01-09 15:13:15 +0100

    add postlists link i navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,14 @@ export default function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <NavLink to="/" className="navbar-brand">HomePage</NavLink>
-
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <NavLink to="/posts" className="nav-link active" aria-current="page">Lista dei posts</NavLink>
+
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Link</a>
